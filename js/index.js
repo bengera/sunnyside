bbc4c3136cc088d100497e7e38a255192be0d5dc
BenGera <61493854+bengera@users.noreply.github.com>
2021-09-01 21:02:47 +0700

    testimonials

---
+++ js/index.js
@@ -0,0 +1,1 @@
+$(window).on('unload', function() { $(window).scrollTop(0); }); window.onunload = function(){ window.scrollTo(0,0); } if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; } 
