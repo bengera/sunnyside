20f875244a1e80743b3916cd6b90e20eea356cde
BenGera <61493854+bengera@users.noreply.github.com>
2021-09-06 15:59:24 +0700

    mobile-nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-$(window).on('unload', function() { $(window).scrollTop(0); }); window.onunload = function(){ window.scrollTo(0,0); } if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; } 
+// $(window).on('unload', function() { $(window).scrollTop(0); }); window.onunload = function(){ window.scrollTo(0,0); } if ('scrollRestoration' in history) { history.scrollRestoration = 'manual'; } 
+'use strict'
+
+const hamburgerNav = document.querySelector('.hamburger-btn')
+const navigation = document.querySelector('.navigation')
+
+hamburgerNav.addEventListener('click', () => {
+    if (navigation.style.display === "none") {
+        navigation.style.display="block";
+        navigation.classList.add('open')
+    } else {
+        navigation.style.display = "none";
+        navigation.classList.remove('open')
+    }
+    
+});
+
+
